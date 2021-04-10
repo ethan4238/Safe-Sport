773227589487f8501d416e18ed0e5ff0066beb7e
ethan4238 <82256639+ethan4238@users.noreply.github.com>
2021-04-10 08:46:56 +0200

    Add files via upload

---
+++ CODE_accel.ino
@@ -0,0 +1,35 @@
+#include "Wire.h"  // Arduino Wire library
+#include "I2Cdev.h"  //bibliothèque I2Cdev à installer
+#include "MPU6050.h" //bibliothèque MPU6050 à installer
+//AD0 low = 0x68 (default for InvenSense evaluation board)
+//AD0 high = 0x69
+MPU6050 mpu6050;
+ 
+int16_t ax, ay, az;  //mesures brutes
+
+ 
+void setup() {
+  Wire.begin();  // bus I2C
+  Serial.begin(9600); // liaison série
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB (LEONARDO)
+  }
+  mpu6050.initialize();  // initialize device
+}
+ 
+void loop() {
+
+  mpu6050.getAcceleration(&ax, &ay, &az);
+  
+ 
+  // Affichage accel/gyro x/y/z
+  Serial.print("a/g:\t");
+  Serial.print(ax); 
+  Serial.print("\t");
+  Serial.print(ay); 
+  Serial.print("\t");
+  Serial.print(az); 
+  Serial.print("\t");
+  Serial.println("\t");
+  delay(100);  
+}
